109d6ebef9ba04ef79e98d996202b12b5f9eaab5
OluwaToyin Agboola <115198993+OluwaToyinG@users.noreply.github.com>
2023-06-23 23:56:44 +0100

    jhfh

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
   theme: {
     extend: { 
       colors:{
-        
-      }
+        'desaturated-red': '#CE9797',
+        'soft-red': '#F96262',
+        'dark-grayish-red': '413A3A',
+
+      },
+
+      fontFamily: {
+        'josefin-sans': ['Josefin Sans', 'sans-serif'],
+      },
       
   },
   },
